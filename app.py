55409cc7e3dbffe161c8dbe72204aac02a82b056
vinitecnico <vinitecnico@gmail.com>
2023-11-26 19:35:49 -0300

    feat: refactory method get by id and delete

--- app.py
+++ app.py
@@ -4,25 +4,25 @@ from services.item_service import ItemService
 app = Flask(__name__)
 item_service = ItemService()
 
+
 @app.route('/items', methods=['GET'])
 def get_items():
     items = item_service.get_all()
-    return jsonify(items)
+    return items
 
-# @app.route('/items/<item_id>', methods=['GET'])
-# def get_item(item_id):
-#     response = dynamodb.get_item(TableName=table_name, Key={'id': {'N': item_id}})
-#     item = response.get('Item')
-#     if item:
-#         return jsonify(item)
-#     return jsonify({'error': 'Item not found'}), 404
+
+@app.route('/items/<item_id>', methods=['GET'])
+def get_item(item_id):
+    item = item_service.get_by_id(int(item_id))
+    if item:
+        return item
+    return jsonify({'error': 'Item not found'}), 404
+
 
 @app.route('/items', methods=['POST'])
 def create_item():
     item_service.create(request.get_json())
-    response = jsonify({'message': 'Item created successfully'})
-    response.status_code = 201
-    return response
+    return jsonify({'message': 'Item created successfully'}), 201
 
 # @app.route('/items/<item_id>', methods=['PUT'])
 # def update_item(item_id):
@@ -31,10 +31,12 @@ def create_item():
 #     response = dynamodb.put_item(TableName=table_name, Item=item)
 #     return jsonify({'message': 'Item updated successfully'})
 
-# @app.route('/items/<item_id>', methods=['DELETE'])
-# def delete_item(item_id):
-#     response = dynamodb.delete_item(TableName=table_name, Key={'id': {'N': item_id}})
-#     return jsonify({'message': 'Item deleted successfully'})
+
+@app.route('/items/<item_id>', methods=['DELETE'])
+def delete_item(item_id):
+    item_service.delete(int(item_id))
+    return jsonify({'message': 'Item deleted successfully'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- readme.md
+++ readme.md
@@ -1,10 +1,23 @@
+#docker
 docker-compose up
 docker-compose down
 
-GET  - All
-curl --location 'http://localhost:5000/items'
 
-POST - create
-curl --location 'http://localhost:5000/items' \
---header 'Content-Type: application/json' \
---data-raw '{"id": 456, "name": "Jane Doe", "age": 25, "email": "janedoe@example.com"}'
+#request
+
+##GET  - All
+```
+    curl --location 'http://localhost:5000/items'
+```
+
+##POST - create
+```
+    curl --location 'http://localhost:5000/items' \
+    --header 'Content-Type: application/json' \
+    --data-raw '{"id": 456, "name": "Jane Doe", "age": 25, "email": "janedoe@example.com"}'
+```
+
+##GET - by id
+```
+    curl --location 'http://localhost:5000/items/1'
+```
